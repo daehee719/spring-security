169b2d05e556e4e1ae4240b37202fc0d34424589
daehee719 <daeheuigang@gmail.com>
2024-10-26 00:25:30 +0900

    1 일차 - formLogin

--- src/main/java/com/example/springsecurity/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/SecurityConfig.java
@@ -1,15 +1,23 @@
 package com.example.springsecurity;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -25,7 +33,31 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                // .formLogin(Customizer.withDefaults()); 기본 로그인 폼
+                .formLogin(form -> form
+//                        .loginPage("/loginPage")
+                        .loginProcessingUrl("/loginProc")
+                        // false로 하면 기존 페이지로 리다이렉션 됨
+                        .defaultSuccessUrl("/", false)
+                        .failureUrl("/fail")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        // 인증되면 /home 이라는 url로 리다이렉션
+                        // 재정의를 하면 위의 default 메서드들을 무시됨
+                        .successHandler((request, response, authentication) ->
+                        {
+                            System.out.println("authentication : " + authentication);
+                            response.sendRedirect("/home");
+                        })
+                        // 이렇게도 가능
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("exception : " + exception.getMessage());
+                            response.sendRedirect("/login");
+                        })
+                        // 인증이 되어있지 않더라도 이동 가능
+                        .permitAll()
+                );
+
 
         return http.build();
     }
